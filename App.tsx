80e85b9082c2ad886612940387a3af91be7e7c96
Xavier Carpentier <contact@xavier-carpentier.com>
2020-09-19 15:15:35 +0200

    status + web

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextProps } from 'react-native';
 
 function Row(props: { children: React.ReactNode }) {
@@ -22,6 +22,7 @@ const Circle = 'o'
 const Cross = 'x'
 const Empty = ' '
 type Symbol = typeof Circle | typeof Cross | typeof Empty
+type Gamer = typeof Cross | typeof Circle
 type GridProps = { tab: Symbol[][], onPress(x: number, y: number): void }
 function Grid(props: GridProps) {
   return (
@@ -65,7 +66,39 @@ function Grid(props: GridProps) {
   );
 }
 
-
+function verify(joueur: Gamer, t: Symbol[][]) {
+  const { stringify: strfy } = JSON
+  const v =
+    strfy(t[0]) === strfy([joueur,joueur,joueur]) || 
+    strfy(t[1]) === strfy([joueur,joueur,joueur]) ||
+    strfy(t[2]) === strfy([joueur,joueur,joueur]) ||
+    (
+      t[0][0]===joueur &&
+      t[1][0]===joueur &&
+      t[2][0]===joueur
+    )
+    ||
+    (
+      t[0][1]===joueur &&
+      t[1][1]===joueur &&
+      t[2][1]===joueur
+    )
+    ||
+    (
+      t[0][2]===joueur &&
+      t[1][2]===joueur &&
+      t[2][2]===joueur
+    )
+    ||
+    (
+      t[0][2]===joueur &&
+      t[1][1]===joueur &&
+      t[2][0]===joueur
+    )
+  
+    
+    return v
+}
 
 export default function App() {
   const [tab, setTab]  = useState<Symbol[][]>([
@@ -74,24 +107,49 @@ export default function App() {
     [Empty, Empty, Empty]
   ]);
 
-  const [gamer, setGamer] = useState<typeof Cross | typeof Circle>(Cross)
+  const [gamer, setGamer] = useState<Gamer>(Cross)
+
+  const [status, setStatus] = useState<Status>("pas-fini")
 
   const onPress = (x: number, y: number) => {
     const copyTab = tab;
     copyTab[x][y] = gamer;
     setTab([copyTab[0], copyTab[1], copyTab[2]]);
+  }
+
+  useEffect(() => {
+    if (verify(Cross, tab)) {
+      setStatus('x_gagne')
+    }
+    if (verify(Circle, tab)) {
+      setStatus('o_gagne')
+    }
+    if(
+      tab[0][0] !== Empty &&
+      tab[0][1] !== Empty && 
+      tab[0][2] !== Empty && 
+      tab[1][0] !== Empty && 
+      tab[1][1] !== Empty && 
+      tab[1][2] !== Empty && 
+      tab[2][0] !== Empty && 
+      tab[2][1] !== Empty && 
+      tab[2][2] !== Empty) {
+        setStatus('plein')
+    }
     if (gamer === Cross) {
       setGamer(Circle)
     } else {
       setGamer(Cross)
     }
-  }
+  }, [tab])
 
   return (
     <View style={styles.container}>
       <Grid tab={tab} onPress={onPress}/>
       <Text>{"\nÀ qui le tour ?"}</Text>
       <Text>{gamer}</Text>
+      <Text>{"\nStatus"}</Text>
+      <Text>{status}</Text>
     </View>
   );
 }
