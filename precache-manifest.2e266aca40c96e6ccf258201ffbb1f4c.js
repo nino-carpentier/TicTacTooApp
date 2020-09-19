9859ca6161832495ad38a9c803e907c0df60d50b
Xavier Carpentier <contact@xavier-carpentier.com>
2020-09-19 14:19:52 +0200

    Updates

---
+++ precache-manifest.2e266aca40c96e6ccf258201ffbb1f4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9233083f918d2978748d",
+    "url": "/static/js/app.57ebee84.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "77b6d9c01c70a0e46083",
+    "url": "/static/js/2.3d4c0680.chunk.js"
+  },
+  {
+    "revision": "f51d8b446193526bc220ddb6dc3ae731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.3d4c0680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "edcd7b37a1b12903cbeca2244fdb8124",
+    "url": "/manifest.json"
+  }
+];
